f5b70861e31176093af1875d4572d6337bb0b6a0
Alejandro <bonilloalejandro@outlook.com>
2024-10-24 14:18:02 -0300

    logo fix

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -13,24 +13,25 @@ export default function CustomNavbar() {
 
   return (
     <Navbar expand="lg" className="navbarcss1 shadow">
-      <a href='/'><Image src={logo} alt="logo" className="navbarlogo m-2" /></a>
-      <Container className=''>
-        <Navbar.Brand href="/">
-          
-        </Navbar.Brand>
+      <div className="navbarlogoa">
+        <a href='/' className='navbarlogoa'>
+          <Image src={logo} alt="logo" className="navbarlogo" />
+        </a>
         <Navbar.Toggle 
           aria-controls="basic-navbar-nav" 
           aria-expanded={expanded}
           onClick={() => setExpanded(!expanded)}
-          className="navbar-toggler"
+          className="navbar-toggler m-3"
         />
+      </div>
+      <Container>
         <Navbar.Collapse id="basic-navbar-nav" className={`collapse navbar-collapse ${expanded ? 'show' : ''}`}>
-          <Nav className="navbarcss fw-bold fs-5 ">
-          <Nav.Item className="navbaroptionswithlogo">
+          <Nav className="navbarcss fw-bold fs-5">
+            <Nav.Item className="navbaroptionswithlogo">
               <Image src={paranormallogo} alt="paranormallogo" className="logosnavbar" />
               <Nav.Link href="/heroeraceparanormal" className="navbarcssitems">Heroes Race Paranormal</Nav.Link>
             </Nav.Item>
-            <Nav.Item className="navbaroptionswithlogo" >
+            <Nav.Item className="navbaroptionswithlogo">
               <Image src={heroeracelogo} alt="heroeracelogo" className="logosnavbar" />
               <Nav.Link href="/heroerace" className="navbarcssitems">Heroes Race</Nav.Link>
             </Nav.Item>
@@ -43,11 +44,8 @@ export default function CustomNavbar() {
               <Nav.Link href="/contacto" className="navbarcssitems">Contacto</Nav.Link>
             </Nav.Item>
           </Nav>
-          
         </Navbar.Collapse>
-        
       </Container>
-      
     </Navbar>
   );
 }
